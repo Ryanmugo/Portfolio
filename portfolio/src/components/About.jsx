ece1c0c589094aa8f48ff8e08ed3f3c24a59c0c7
Ryan Mugo <gathariryan@gmail.com>
2024-01-16 15:35:17 +0100

    EDITS of my work completed

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -13,30 +13,17 @@ const About = () => {
           <h1 className="text-3xl sm:text-5xl font-bold text-white mb-6 mt-6">
             Ryan: The Full Stack Developer Extraordinaire <br />
           </h1>
-          <p className="text-xl mt-17">
-            In a world where digital experiences define the way we interact, I,
-            Ryan, am your Full Stack Developer Extraordinaire, transforming
-            visions into seamless realities. With my mastery of React, MongoDB,
-            Express, JavaScript, TypeScript, Node.js and Playwright Testing Kit
-            By Microsoft, I seamlessly blend creativity and technical prowess to
-            craft exceptional web applications. My passion for innovation fuels
-            my dedication to crafting user-centric experiences that captivate
-            and engage. I approach every project with an unwavering commitment
-            to quality, ensuring that your vision is not just met but exceeded.
-            Whether you&apos;re seeking a dynamic e-commerce platform, an
-            intuitive project management tool, or a sophisticated enterprise
-            application, I&apos;m your trusted partner in translating your ideas
-            into tangible solutions. My expertise extends beyond the coding
-            realm, encompassing a deep understanding of user experience, design
-            principles, and the latest web development trends. I&apos;m not just
-            a developer; I&apos;m a problem solver, a creative thinker, and a
-            passionate advocate for your digital success. Let me be your key to
-            unlocking the boundless potential of the digital landscape.
+          <p className="text-xl mt-17 mb-20">
+            I blend creativity with technical prowess to craft outstanding web
+            applications, ensuring your vision exceeds expectations. Whether you
+            need an e-commerce platform, project management tool, or enterprise
+            application, I&apos;m your trusted partner. Beyond coding, I bring a
+            deep understanding of user experience, design principles, and the
+            latest web development trends. I&apos;m not just a developer;
+            I&apos;m a problem solver and a passionate advocate for your digital
+            success. Let me unlock the boundless potential of the digital
+            landscape for you.
           </p>
-
-          <h1 className="mt-6 text-2xl underline">
-            Contact me today to discuss how I can bring your vision to life.
-          </h1>
 
           <br />
         </div>
---
+++ portfolio/src/components/Experience.jsx
@@ -0,0 +1,107 @@
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import mongodb from "../assets/mongodb.jpg";
+import node from "../assets/node.png";
+import react from "../assets/react.png";
+import github from "../assets/github.png";
+import tailwind from "../assets/tailwind.png";
+import typescript from "../assets/typescript.png";
+import express from "../assets/express.jpg";
+
+const Experience = () => {
+  const techs = [
+    {
+      id: 1,
+      src: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: typescript,
+      title: "TypeScript",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 5,
+      src: react,
+      title: "HTML",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 6,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+    {
+      id: 7,
+      src: mongodb,
+      title: "MongoDB",
+      style: "shadow-green-400",
+    },
+    {
+      id: 8,
+      src: express,
+      title: "Express",
+      style: "shadow-gray-400",
+    },
+    {
+      id: 9,
+      src: node,
+      title: "Node.js",
+      style: "shadow-green-400",
+    },
+    {
+      id: 10,
+      src: github,
+      title: "Github",
+      style: "shadow-gray-400",
+    },
+  ];
+
+  return (
+    <div
+      name="experience"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+    >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div className="">
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            Experience
+          </p>
+          <p className="py-6">
+            These are the technologies I&apos;ve worked with
+          </p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+          {techs.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt="" className="w-11 mx-auto mb-2" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
